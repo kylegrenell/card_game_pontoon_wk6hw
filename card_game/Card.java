ac2ee4ad73edf82fbc7397a3d83b1d945b16f2fb
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-11-06 12:01:16 +0000

    card and card tests added

---
+++ card_game/Card.java
@@ -0,0 +1,12 @@
+package card_game;
+
+public class Card {
+ private Suit suit;
+ private Rank rank;
+
+ public Card(Suit suit, Rank rank){
+  this.suit = suit;
+  this.rank = rank;
+ } 
+}
+
---
+++ card_tests/CardTest.java
@@ -0,0 +1,8 @@
+import card_game.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class CardTest {
+  private Card card;
+  private Hand hand;
+}
