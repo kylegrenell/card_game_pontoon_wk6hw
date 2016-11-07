4e4677099aacbcf2a36207fac945a2cf0319998c
kyle.grenell@gmail.com <kyle.grenell@gmail.com>
2016-11-07 08:33:02 +0000

    notes aded game test

--- card_tests/GameTest.java
+++ card_tests/GameTest.java
@@ -3,32 +3,37 @@ import org.junit.*;
 import card_game.*;
 
 public class GameTest {
-     Game game;
-     Player player;
-     Player gameDealer;
-     Hand hand;
-     Deck deck;
-     Card card1;
-     Card card2;
+Game game;
+Player player;
+Player gameDealer;
+Hand hand;
+Deck deck;
+Card card1;
+Card card2;
 
-     @Before
-     public void before(){
-          game = new Game();
-          deck = new Deck();
-          player = new Player();
-          gameDealer = new Player();
-          player.hand = new Hand();
-          card1 = new Card(Suit.DIAMONDS, Rank.TEN);
-          card2 = new Card(Suit.HEARTS, Rank.QUEEN);
-          deck.addCard(card1);
-          deck.addCard(card2);
-     }
+@Before
+public void before(){
+     // game and deck of cards to be dealt
+     game = new Game();
+     deck = new Deck();
 
-     @Test
-     public void canDealHand() {
-          game.dealHand(deck, player);
-          assertEquals(2, player.hand.handSize());
-     }
+     // players and their hand
+     player = new Player();
+     gameDealer = new Player();
+     player.hand = new Hand();
+
+     // cards being dealt
+     card1 = new Card(Suit.DIAMONDS, Rank.TEN);
+     card2 = new Card(Suit.HEARTS, Rank.QUEEN);
+     deck.addCard(card1);
+     deck.addCard(card2);
+}
+
+@Test
+public void canDealHand() {
+     game.dealHand(deck, player);
+     assertEquals(2, player.hand.handSize());
+}
 
 
 }
